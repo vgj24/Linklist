267563b1d0f50b6d223bb3fa822cac8bd32558bb
vgj24 <vrushlijoshi24@gmail.com>
2022-01-20 11:00:36 +0530

    link list

---
+++ LinkListProgramAssgn/LinkLIstAssign.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkLIstProgramAssgn
+{
+     class LinkLIstAssign:IComparable
+    {
+        internal Node head;
+
+        public int CompareTo(object? obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if(this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next= node;
+            }
+            Console.WriteLine("{0} is added to linklist",node.data);
+        }
+
+        internal void Display()
+        {
+            Console.Write("List =");
+            Node temp = this.head;
+            int count = 0;
+            if (temp == null) 
+            {
+                Console.WriteLine("liinked list is empty");
+            }
+            else
+            {
+                while(temp != null)
+                {
+                    
+                    Console.Write(temp.data+" ");
+                    count=count+1;
+                    temp=temp.next;
+                }
+                Console.Write(" size: " + count++);
+
+            }
+
+        }
+        internal Node insertAtPerticularPosition(int position,int data)
+        {
+            if(position<1)
+            {
+                Console.WriteLine("invalid position");
+            }
+            if(position==1)
+            {
+                var newnode=new Node(data);
+                newnode.next= this.head;
+                head=newnode;
+            }
+            else
+            {
+                Node temp=this.head;
+                while(position-- !=0)
+                {
+                    if(position == 1)
+                    {
+                        Node node=new Node(data);   
+                        node.next= this.head.next;
+                        head.next= node;
+
+                        break;
+                    }
+                    temp=temp.next;
+                }
+                if(position!=1)
+                {
+                    Console.WriteLine("position is out of range");
+                }
+
+            }
+            return head;        
+        }
+        internal Node removeFirstnode()
+        {
+            if(head == null)
+                return null;
+
+            this.head=this.head.next;
+            return this.head;
+        }
+        internal Node removeLastnode()
+        {
+            if (head == null)
+                return null;
+            if (head.next == null)
+                return null;
+            Node newnode = head;
+            while(newnode.next.next != null)
+            {
+                newnode = newnode.next;
+            }
+            newnode.next = null;
+            return head;
+        }
+        internal Node Search(int value)
+        {
+            while (this.head != null)
+            {
+                if (this.head.data == value)
+                {
+                    return this.head;
+                    //break;
+                }
+                this.head = this.head.next;
+
+            }
+            return null;
+        }
+        internal Node Searchnew()
+        {
+                var v= Search(30);
+                if (v != null)
+                { 
+                    insertAtPerticularPosition(2,40);
+                }
+            return null;
+        }
+        internal void Organizedlist()
+        {
+            //if(this.head==null)
+            //Console.WriteLine("list is empty");
+            //  return null;
+            //if (this.head.next == null)
+            //  return null;
+            Node Current = head;
+            int temp1;
+            Node newnode = null;
+
+            if (this.head == null)
+            {
+                Console.WriteLine("list is empty");
+                return;
+            }
+            else
+            {
+                while (Current != null)
+                {
+                    newnode = Current.next;
+                    while (newnode != null)
+                    {
+                        if (Current.data.CompareTo(newnode.data) > 0)
+                        {
+                            temp1 = Current.data;
+                            Current.data = newnode.data;
+                            newnode.data = temp1;
+                        }
+                        newnode = newnode.next;
+                    }
+                    Current = Current.next;
+                }
+            }
+        } 
+}    }   
+                   // temp = this.head;
+                   // if (temp.data.CompareTo(newnode.data) >= 0)
+                   // {
+                   // this.head.data = newnode.data;
+                   // newnode.data =temp.data;
+                   //// temp.next = this.head.next;
+                   // //newnode = temp.next;
+                   // Console.WriteLine(" head " + this.head.data);
+                   // Console.WriteLine(" temp" + temp.data);
+                   // //Console.WriteLine(" new node " + newnode.data);
+                   // }
+                   // else //56-30 false then 56 becomes head.data and 30 becomes this.head.next
+                   // {
+                   // this.head.data = temp.data;
+                   // temp.next = this.head.next;
+                   // newnode = temp.next;
+                   // Console.WriteLine(" head " + this.head.data);
+                   // Console.WriteLine("temp  " + temp.data);
+
+                    //Console.WriteLine(" newnode" + newnode.data);
+            
+                //    temp=this.head.next;
+                //    newnode=temp.next;
+                //    Console.WriteLine(" "+temp.data);
+                //}
+
+               // this.head = this.head.next;
+           // }
+         //   return null;
+       // }
+   // }
+//}
---
+++ LinkListProgramAssgn/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LinkLIstProgramAssgn;
+
+Console.WriteLine("Link List Assignment");
+#region linklist
+LinkLIstAssign list = new LinkLIstAssign();
+//Console.WriteLine("Adding 56,30,70 to linked list");
+//list.Add(56);
+//list.Add(30);
+//list.Add(70);
+//list.Display();
+
+//Console.WriteLine("adding 70,,30 ,56 t list ");
+//list.Add(70);
+//list.Add(56);
+//list.Display();
+
+//Console.WriteLine("Adding 56 then 30 then 70");
+//list.Add(56);
+//list.Add(30);
+//list.Add(70);
+
+//Console.WriteLine("adding 56 and 70 to list");
+//list.Add(56);
+//list.Add(70);
+//list.Display();
+//Console.WriteLine("\n adding at 30 between 56 and 70 perticuler position", list.insertAtPerticularPosition(2, 30));
+//list.Display();
+//Console.WriteLine("\n Searching node 30");
+//list.Search(30);
+//Console.WriteLine("\n adding 40 after 30 ", list.Searchnew());
+//list.Display();
+//Console.WriteLine("\n remove 40 from list ");
+//list.Search(40);
+
+//list.removeFirstnode();
+//list.Display();
+//Console.WriteLine("\n");
+list.Add(56);
+list.Add(30);
+list.Add(70);
+list.Display();
+
+//Console.WriteLine("\n Remove first element ", list.removeFirstnode());
+//list.Display();
+
+//Console.WriteLine("\n Remove last node from list", list.removeLastnode());
+//list.Display();
+
+Console.WriteLine("\n organized list is ");
+list.Organizedlist();
+list.Display();
+
+Console.ReadKey();
+
+#endregion
